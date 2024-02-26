e8a80306bf43f474b0ece7f31acdc2488c53f24b
jay menorca <zaimenorca@gmail.com>
2024-02-26 18:51:09 -0500

    214; moved child component to external module

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Jay's proj repo for Springboot 3, Spring Framework 6, React Training (in vscode)
 
 proj: react-todoapp-demo
 
+- moved child component to external module
 - added parent component to demo global variable, function, accessed in child component
 - added Properties/props, constraints using propTypes(import from prop-types'), default properties (defaultProps)
 - added increment, decrement, reset
--- react-todoapp-demo/src/components/counter/counter.jsx
+++ react-todoapp-demo/src/components/counter/counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './counter.css'
-import PropTypes from 'prop-types'
+import CounterButton from './counterbutton';
 
 export default function Counter(){
     const [totcount, setCount] = useState(0);
@@ -19,46 +19,3 @@ export default function Counter(){
     )
 }
 
-function CounterButton({by, parentCounter, totcount}){
-
-    const [count, setCount] = useState(0);
-
-    function counterPlusPlus(){
-        setCount(count+by)
-        parentCounter(by)
-    }
-
-    function decrement(){
-        setCount(count-by)
-        var byvar = -1 * by
-        parentCounter(byvar)
-    }
-
-    function counterZero(){
-        setCount(0)
-        var byvar = -1 * totcount
-        parentCounter(byvar)
-    }
-
-    return(
-        <div className="Counter">
-            <span className="count">{count}</span>
-            <div><button className="countBtn" onClick={counterPlusPlus}
-            >+{by}</button>
-            <button className="countBtn" onClick={decrement}
-            >-{by}</button>
-            <button className="countBtn" onClick={counterZero}
-            >reset</button>
-            </div>
-        </div>
-    )
-
-}
-
-CounterButton.propTypes = {
-    by: PropTypes.number
-}
-
-CounterButton.defaultProps = {
-    by: 1
-}
